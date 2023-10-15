42aeb98865213794c40f6b7a8e2b5f532bc5adca
Selam Ararsa <selamkedir@icloud.com>
2023-10-15 21:03:18 +0100

    page refresh fixed

--- main.js
+++ main.js
@@ -1,125 +1,105 @@
-const startPage = document.getElementById("main-page");
-const quizPage = document.getElementById("quiz-page");
-const quizForm = document.getElementById("quizForm")
-const resultPage = document.getElementById("results-page");
-const startButton = document.getElementById("start-button");
-const submitButton = document.getElementById("submit-button")
-const goBackButton = document.getElementById("startover-button")
-// quiz questions and choices 
+const startPage = document.getElementById('main-page');
+const quizPage = document.getElementById('quiz-page');
+const quizForm = document.getElementById('quizForm');
+const resultPage = document.getElementById('results-page');
+const startButton = document.getElementById('start-button');
+const submitButton = document.getElementById('submit-button');
+const displayQuestion = document.getElementById('question');
 
-const questionsAndChoices = [
-    {
-        question: "Are you a lone wolf or a team player?",
-        choices:[
-            {text: "Lone wolf"},
-            {text: "Team player"}
-        ]
-    },{
-        question:"Would you prefer an easy but safe character or a difficult charachter to master?",
-        choices:[
-            {text: "Easy"},
-            {text: "Difficult"}
-        ]
-    },
-    {
-        question:"Which region interestes you most?",
-        choices:[
-            {text: "Freljord"},
-            {text: "Piltover"},
-            {text: "Shurima"},
-            {text: "Noxus"},
-            {text: "Zaun"},
-            {text: "Targan"},
-            {text: "Bandle city"},
-            {text: "Lonia"}
-        
-        ]
-    },
-    {
-        question:"Which role do you want to play?",
-        choices:[
-            {text: "Top"},
-            {text: "Jungle"},
-            {text: "Mid"},
-            {text: "Botlane"}
-        
-        ]
-    },
-    {
-        question:"Do you prefer to keep enemies at range or get up close and personal ?",
-        choices:[
-            {text: "At range"},
-            {text: "Close"},
-        
-        ]
-    },
-
-]
-// console.log(questionsAndChoices[3].choices[2]);
-
-
-
-
-// on click change pages
-
-startButton.addEventListener("click", ()=>{
-    startPage.style.display = "none";
-    quizPage.style.display = "block";
-
-    // call a function that displays the quiz container with questions and choices 
-    generateQuiz()
-  
-})
-
-
-
-const displayQuestion = document.getElementById("question");
-const choiceButtons = document.getElementById("choice-buttons");
-
-let currentQuestionIndex = 0; 
+const choiceButtons = document.getElementById('choice-buttons');
+console.log(choiceButtons);
+let currentQuestionIndex = 0;
 let userResponse = [];
 
-//display questions
- function generateQuiz () {
-    let currentQuestion = questionsAndChoices[currentQuestionIndex];
-    let questionNo = currentQuestionIndex + 1;
-    displayQuestion.innerHTML = questionNo +  ". " +  currentQuestion.question;
+// Store questions and choices in an object
 
-    //clear all buttons
-    choiceButtons.innerHTML = '';
-    //display choices
+const questionsAndChoices = [
+  {
+    question: 'Are you a lone wolf or a team player?',
+    choices: [{ text: 'Lone wolf' }, { text: 'Team player' }],
+  },
+  {
+    question:
+      'Would you prefer an easy but safe character or a difficult character to master?',
+    choices: [{ text: 'Easy' }, { text: 'Difficult' }],
+  },
+  {
+    question: 'Which region interestes you most?',
+    choices: [
+      { text: 'Freljord' },
+      { text: 'Piltover' },
+      { text: 'Shurima' },
+      { text: 'Noxus' },
+      { text: 'Zaun' },
+      { text: 'Targan' },
+      { text: 'Bandle city' },
+      { text: 'Lonia' },
+    ],
+  },
+  {
+    question: 'Which role do you want to play?',
+    choices: [
+      { text: 'Top' },
+      { text: 'Jungle' },
+      { text: 'Mid' },
+      { text: 'Botlane' },
+    ],
+  },
+  {
+    question:
+      'Do you prefer to keep enemies at range or get up close and personal ?',
+    choices: [{ text: 'At range' }, { text: 'Close' }],
+  },
+];
+startButton.addEventListener('click', () => {
+  // Hide the Start Page
+  startPage.style.display = 'none';
 
-    currentQuestion.choices.forEach(choice =>{
-        const button = document.createElement("button");
-        button.innerHTML = choice.text;
-        button.classList.add("choice-btn");
-        choiceButtons.appendChild(button);
+  // Show the Quiz Page
+  quizPage.style.display = 'block';
+  resultPage.style.display = 'none';
+  // Call a function to load and display quiz questions
+  runQuiz();
+});
 
-        button.addEventListener("click", choiceSelected)
-    })
+// write a function to load quiz questions and choices
+
+function runQuiz() {
+  let currentQuestion = questionsAndChoices[currentQuestionIndex];
+  console.log(currentQuestion);
+  //assign question number to each question
+  let questionNumber = currentQuestionIndex + 1;
+
+  //display question inside the 'question' h2 element
+  displayQuestion.innerHTML = questionNumber + '. ' + currentQuestion.question;
+
+  //set the choice buttons to empty to start with
+  choiceButtons.innerHTML = '';
+
+  //create buttons for each multiple choice options
+
+  currentQuestion.choices.forEach((choice, index) => {
+    const button = document.createElement('button');
+    button.textContent = choice.text;
+    button.classList.add('choice-btn');
+    choiceButtons.appendChild(button);
+
+    button.addEventListener('click', choiceSelected);
+  });
+  submitButton.disabled = true;
 }
- function choiceSelected (event){
-    
-    const selectedChoice = event.target.textContent;
 
-    //check if there are more questions and move to the next one
-    if(currentQuestionIndex < questionsAndChoices.length - 1){
-        currentQuestionIndex++;
-      generateQuiz()
-      userResponse.push(selectedChoice);
-   
-    }else{
-        startPage.style.display = "none";
-        quizPage.style.display = "none";
-        resultPage.style.display = "block"
-    }
-   
- }
+function choiceSelected(event) {
+  const selectedChoice = event.target.textContent;
 
-
-
-
-
-
-
-
+  //check if there are more questions and move to the next one
+  if (currentQuestionIndex < questionsAndChoices.length) {
+    currentQuestionIndex++;
+    runQuiz();
+    userResponse.push(selectedChoice);
+  } else {
+    submitButton.disabled = false;
+    // quizPage.style.display = 'none';
+    // resultPage.style.display = 'block';
+  }
+}
